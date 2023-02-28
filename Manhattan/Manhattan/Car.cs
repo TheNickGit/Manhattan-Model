d5f9e50508efef552618088ba7775797cfd4e3a8
kdkoerts <kdkoerts@users.noreply.github.com>
2023-02-28 13:58:28 +0100

    Update Car.cs
    Timer added

--- Manhattan/Manhattan/Car.cs
+++ Manhattan/Manhattan/Car.cs
@@ -6,8 +6,13 @@ internal class Car
     Crossing startlocation;
     Crossing destination;
     Route route;
-
+    
     // Data voor tijd etc.
+    long startTime = Stopwatch.GetTimestamp();
+
+    TimeSpan elapsedTime = Stopwatch.GetElapsedTime(startTime);
+    
+
 
     // Houd route bij en wordt voortbewogen
 }
